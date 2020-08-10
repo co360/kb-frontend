4e094f7a8646c2de8c8005da6acca3fb03613c55
Samuel García <samuelluciogar@gmail.com>
2020-08-10 00:00:21 -0500

    Updates

---
+++ precache-manifest.fe2df6123b0edd78891dd3f43ff31cc8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529a9d972fe2f780d54bc526290e4611",
+    "url": "/kb-frontend/index.html"
+  },
+  {
+    "revision": "7e58fc2e00cca99690c9",
+    "url": "/kb-frontend/static/css/main.5341c381.chunk.css"
+  },
+  {
+    "revision": "560cb465772675d2e685",
+    "url": "/kb-frontend/static/js/2.6ae6b591.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kb-frontend/static/js/2.6ae6b591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e58fc2e00cca99690c9",
+    "url": "/kb-frontend/static/js/main.56b06ee3.chunk.js"
+  },
+  {
+    "revision": "e2e0c744586d69b7d771",
+    "url": "/kb-frontend/static/js/runtime-main.53fd3f5a.js"
+  },
+  {
+    "revision": "12c5745f177d59ee4fedd780f5a47cc2",
+    "url": "/kb-frontend/static/media/computer.12c5745f.png"
+  },
+  {
+    "revision": "d8e6b5f2d103138349b5c1f795eaf193",
+    "url": "/kb-frontend/static/media/healthy-body.d8e6b5f2.jpg"
+  },
+  {
+    "revision": "aed1bb69d7529dc86b7618687e61c2b5",
+    "url": "/kb-frontend/static/media/healthy-lose.aed1bb69.jpg"
+  },
+  {
+    "revision": "b9186efa6d05d5bff09d7a6540b8e4e5",
+    "url": "/kb-frontend/static/media/healthy-mind.b9186efa.jpg"
+  },
+  {
+    "revision": "ba34d0d160eff1c078662b709dbafc84",
+    "url": "/kb-frontend/static/media/healthy-pregnant.ba34d0d1.jpg"
+  },
+  {
+    "revision": "0346618c60cce8f14abad1cad33d031f",
+    "url": "/kb-frontend/static/media/logo.0346618c.jpeg"
+  },
+  {
+    "revision": "d36b40f663953b59aea585d297e2be2d",
+    "url": "/kb-frontend/static/media/profile-picture.d36b40f6.jpeg"
+  },
+  {
+    "revision": "5af8892ad9e24732e97408329ec4bab3",
+    "url": "/kb-frontend/static/media/sport.5af8892a.png"
+  }
+]);
